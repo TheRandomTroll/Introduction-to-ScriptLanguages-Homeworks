51668693bdee4d6e9973fc5abc7f7444bd7ace39
Mihail Gerginov <Mihail Gerginov>
2016-12-20 17:55:22 +0200

    Added some validation functions. Changed input and board printing.

--- TicTacToe/2Players/TicTacToe.py
+++ TicTacToe/2Players/TicTacToe.py
@@ -3,40 +3,46 @@ import sys
 
 def tictactoe_2_players():
     def print_board(board):
-        for row in board:
-            print(row)
+        print('   |   |')
+        print(' ' + board[0] + ' | ' + board[1] + ' | ' + board[2])
+        print('   |   |')
+        print('-----------')
+        print('   |   |')
+        print(' ' + board[3] + ' | ' + board[4] + ' | ' + board[5])
+        print('   |   |')
+        print('-----------')
+        print('   |   |')
+        print(' ' + board[6] + ' | ' + board[7] + ' | ' + board[8])
+        print('   |   |')
 
 
-    board = [
-        ['-', '-', '-'],
-        ['-', '-', '-'],
-        ['-', '-', '-']
-    ]
+    board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
 
+    print_board(board)
     player = 'X'
     opponent = 'O'
 
-    while True:
-        player_1 = map(int, input("Enter player 1's input coordinates (separated by space): ").split(' '))
-        if board[player_1[0]][player_1[1]] != '-':
-            board[player_1[0]][player_1[1]] = player
-            print_board(board)
-            break
-        else:
-            print("The current cell is already occupied. Enter different coordinates.")
+    def getPlayerMove(board):
+        move = ' '
+        while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
+            print('What is your next move? (1-9)')
+            move = input()
+        return int(move) - 1
 
-    while True:
-        player_1 = map(int, input("Enter player 2's input coordinates (separated by space): ").split(' '))
-        if board[player_1[0]][player_1[1]] != '-':
-            board[player_1[0]][player_1[1]] = opponent
-            print_board(board)
-            break
-        else:
-            print("The current cell is already occupied. Enter different coordinates.")
 
-            
+    def isSpaceFree(board, coord):
+        return board[coord] == ' '
+    
+
+    def is_board_full(board):
+     for i in range(0, 9):
+         if isSpaceFree(board, i):
+             return False
+     return True
+
+
 def main():
-    pass
+    tictactoe_2_players()
 
 if __name__ == "__main__":
     sys.exit(int(main() or 0))
