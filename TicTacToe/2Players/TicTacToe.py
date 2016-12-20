759f4d2439b2e9d37a9488f07b5d573d54f76739
Mihail Gerginov <Mihail Gerginov>
2016-12-20 17:26:54 +0200

    Added Tic-Tac-Toe/2Players folder. Added TicTacToe.py

---
+++ TicTacToe/2Players/TicTacToe.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def main():
+    board = [
+        ['-', '-', '-'],
+        ['-', '-', '-'],
+        ['-', '-', '-'],
+    ]
+
+
+
+
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
