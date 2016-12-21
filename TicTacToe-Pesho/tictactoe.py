4103a4a2297216e52d288977885089b8f584f120
Pejo-306 <penikolov23@gmail.com>
2016-12-21 23:10:55 +0200

    Combine functions into class

--- TicTacToe-Pesho/tictactoe.py
+++ TicTacToe-Pesho/tictactoe.py
@@ -1,10 +1,4 @@
-def player_input():
-    pass
-
-
-def check_win():
-    pass
-
+# Next step: combine functions into a class
 
 def print_board(board):
     print('-' * 9, end='')
@@ -14,6 +8,40 @@ def print_board(board):
     print()
 
 
+def player_turn(board, player):
+    print("Current board: ")
+    print_board(board)
+
+    while True:
+        print("Please enter the row and column")
+        row = int(input("row (1-3): "))
+        column = int(input("column (1-3): "))
+
+        # Check if the position is valid and not occupied
+        if 1 <= row <= 3 and 1 <= column <= 3 and \
+                board[row-1][column-1] == ' ':
+            break
+
+    board[row-1][column-1] = player
+
+
+def check_win(board, player, player_pos):
+    # player_pos = (row, column)
+    row_pos = player_pos[0]
+    column_pos = player_pos[1]
+
+    column = board[0][column_pos], board[1][column_pos], board[2][column_pos]
+    diagonal_1 = board[0][2], board[1][1], board[2][0]
+    diagonal_2 = board[0][0], board[1][1], board[2][2]
+
+    if all(pos == player for pos in diagonal_1) or \
+            all(pos == player for pos in diagonal_2) or \
+            all(pos == player for pos in board[row_pos]) or \
+            all(pos == player for pos in column):
+        return True
+    return False
+
+
 def main():
     board = [
         [' ', ' ', ' '],
@@ -21,6 +49,9 @@ def main():
         [' ', ' ', ' '],
     ]
 
+    player_turn(board, 'x')
+
+    print("End of game")
     print_board(board)
 
 
